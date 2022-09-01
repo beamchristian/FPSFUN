5f60ce821b84a54435eb3c59c63a7afee7943d94
beamchristian <ckbeamcoding@gmail.com>
2022-08-31 20:03:44 -0400

    double jump logic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,13 +7,22 @@ public class PlayerController : MonoBehaviour
     [Header("Controller Settings")]
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float gravityModifier = 5f;
-    [SerializeField] private Transform cameraTransform;
+    [SerializeField] private float jumpPower = 5f;
+
 
     [Header("Mouse Settings")]
     [SerializeField] private float mouseSensitivity = 1.5f;
     [SerializeField] private bool invertX;
     [SerializeField] private bool invertY;
 
+    [Header("References")]
+    [SerializeField] private Transform cameraTransform;
+    [SerializeField] private Transform groundCheckPoint;
+    [SerializeField] private LayerMask whatIsGround;
+
+
+    private bool canJump;
+    private bool canDoubleJump;
     private CharacterController characterController;
     private Vector3 moveInput;
     
@@ -24,9 +33,15 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        HandleCameraRotation();
+        HandleJumping();
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
         // store y velocity
         float yStore = moveInput.y;
-
 
         Vector3 verticalMovement = transform.forward * Input.GetAxisRaw("Vertical");
         Vector3 horizontalMovement = transform.right * Input.GetAxisRaw("Horizontal");
@@ -34,26 +49,52 @@ public class PlayerController : MonoBehaviour
         moveInput = horizontalMovement + verticalMovement;
         moveInput.Normalize();
         moveInput *= moveSpeed;
-        
+
         moveInput.y = yStore;
 
         moveInput.y += Physics.gravity.y * gravityModifier * Time.deltaTime;
 
-        if(characterController.isGrounded)
+        if (characterController.isGrounded)
         {
             moveInput.y = Physics.gravity.y * gravityModifier * Time.deltaTime;
         }
+    }
+
+
+    private void HandleJumping()
+    {
+        canJump = Physics.OverlapSphere(groundCheckPoint.position, .25f, whatIsGround).Length > 0;
+
+        if(canJump)
+        {
+            canDoubleJump = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
+        {
+            moveInput.y = jumpPower;
+
+            canDoubleJump = true;
+        } 
+        else if (canDoubleJump && Input.GetKeyDown(KeyCode.Space))
+        {
+            moveInput.y = jumpPower;
+
+            canDoubleJump = false;
+        }
 
         characterController.Move(moveInput * Time.deltaTime);
+    }
 
-        // control camera rotation
+    private void HandleCameraRotation()
+    {
         Vector2 mouseInput = new(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y") * mouseSensitivity);
 
-        if(invertX)
+        if (invertX)
         {
             mouseInput.x = -mouseInput.x;
         }
-        if(invertY)
+        if (invertY)
         {
             mouseInput.y = -mouseInput.y;
         }
